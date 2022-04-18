8a5904ebf9af5d1624eacabe6ad8f1539d7c4725
しゃかな 沖山 <okiyama1609@yahoo.co.jp>
2022-04-18 13:22:50 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2A_05_オキヤマ_クニアキ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
